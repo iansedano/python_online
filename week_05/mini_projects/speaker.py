9f969837461d3ab0f41bcece898270271bbbaf40
martin-martin <breuss.martin@gmail.com>
2018-06-22 11:34:19 +0200

    update labs

---
+++ week_05/mini_projects/speaker.py
@@ -0,0 +1,9 @@
+'''
+Using the external python module 'markovbot' to create short text snippets
+of your favorite text resource.
+
+https://github.com/esdalmaijer/markovbot
+
+BONUS: extend it to let the bot tweet wisdom once in a while!
+
+'''
