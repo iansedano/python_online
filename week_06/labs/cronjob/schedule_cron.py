5f9577e29f889d5bd69b48cacf27ef81245fe227
martin-martin <breuss.martin@gmail.com>
2018-08-14 10:24:29 +0200

    add CRON exercise

---
+++ week_06/labs/cronjob/schedule_cron.py
@@ -0,0 +1,23 @@
+'''
+Using `python-crontab`, create a CRON job that is basically a clock that
+writes the date and time every minute to a file called `minutes.txt`.
+
+Sample Output:
+
+2018-08-14 10:09:00.572643
+2018-08-14 10:10:00.572643
+
+This exercise is focused on using crontab, therefore a script called
+`time-ticker.py` is already provided. You need to correctly register a
+CRON job programmatically from within this file, and make sure that the
+output is what (and where!) you expect it.
+
+
+HINTS:
+We need to include the full path to our python3 executable, as well as
+to the .py file: https://stackoverflow.com/a/44217854/5717580
+
+If you are not getting what you expect, go to your terminal and
+check your `mail` to find out what's going on with CRON.
+
+'''
