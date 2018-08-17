7b0d1358f40b401cd37f19a87221d63172d688ad
martin-martin <breuss.martin@gmail.com>
2018-08-17 08:30:55 +0200

    fix aggregate functs

---
+++ week_05/labs/04_aggregate_functions/Exercise_02.py
@@ -0,0 +1,11 @@
+'''
+User python's built-in .enumerate() function to print the classes
+together with their numbers from 1-4.
+
+
+NOTE: a less readable, but common structure in other languages would be:
+
+for i in range(len(courses)):
+    print(f"{i}: {courses[i]} python")
+
+'''
