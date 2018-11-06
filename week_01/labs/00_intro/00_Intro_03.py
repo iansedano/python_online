b9c3a1638f38d931274134d96fce01de12b8d6c9
Ian <iansedano@gmail.com>
2018-11-06 12:12:57 +0100

    finishing week 1 labs

--- week_01/labs/00_intro/00_Intro_03.py
+++ week_01/labs/00_intro/00_Intro_03.py
@@ -1,10 +1,10 @@
 '''
 Write the necessary code to display the follow message to the console
- 
+
 	Hello World!!
 	I'm a programmer now.
 	Yeehaw!
 
 '''
 
-print ('Hello World !!/n
+print ('Hello World !!\nI\'m a programmer now.\nYeehaw!')
