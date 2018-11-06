f3d4727356fe1a5f8986513ca3bda60d49e0b49f
Ian <iansedano@gmail.com>
2018-11-06 18:07:46 +0100

    getting started on week 02

--- week_02/labs/03_variables_statements_expressions/Exercise_04.py
+++ week_02/labs/03_variables_statements_expressions/Exercise_04.py
@@ -3,3 +3,14 @@ Write the necessary code that prints out the area and circumference
 of a circle with a radius of 3.14
 
 '''
+
+pi = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+
+r = 3.14
+c = (r * 2) * pi
+
+print(c)
+
+area = pi * (r^2)
+print(area)
+
--- week_02/labs/03_variables_statements_expressions/Exercise_06.py
+++ week_02/labs/03_variables_statements_expressions/Exercise_06.py
@@ -3,3 +3,13 @@ If a runner runs 12 kilometers in 30 minutes and 30 seconds.
 What is his/her average speed in miles per hour. (Tip: 1 mile = 1.6 km)
 
 '''
+
+distance = 12
+
+time = (30 * 60) + 30
+
+miles = distance / 1.6
+
+hour = (time / 60 ) / 60
+
+...need to finish
