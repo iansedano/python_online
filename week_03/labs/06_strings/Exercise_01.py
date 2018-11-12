547b1884f35cbc0c6d1f0a24a54c706bf4970d66
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-11-12 12:52:06 +0100

    string labs updated with directions

--- week_03/labs/06_strings/Exercise_01.py
+++ week_03/labs/06_strings/Exercise_01.py
@@ -1,4 +1,18 @@
 '''
-Complete Exercise 8.2 (p.79) from the textbook.
+You might want to experiment with some of them to make sure you
+understand how they work. strip and replace are particularly useful.
+
+The documentation uses a syntax that might be confusing.
+For example, in find(sub[, start[, end]]), the brackets indicate
+optional arguments. So sub is required, but start is optional, and if
+you include start, then end is optional.
+
+Demonstrate below:
+- strip
+- replace
+- find
+
+Source: Exercise in chapter "Strings" in Think Python 2e:
+http://greenteapress.com/thinkpython2/html/thinkpython2009.html
 
 '''
--- week_03/labs/06_strings/Exercise_02.py
+++ week_03/labs/06_strings/Exercise_02.py
@@ -1,4 +1,18 @@
 '''
-Complete Exercise 8.3 (p.79) from the textbook.
+There is a string method called 'count' that is similar to the following
+code:
+
+word = 'banana'
+count = 0
+for letter in word:
+    if letter == 'a':
+        count = count + 1
+print(count)
+
+Read the documentation of the 'count' method and write an invocation that
+counts the number of a’s in 'banana' that uses the in-built 'count'.
+
+Source: Exercise in chapter "Strings" in Think Python 2e:
+http://greenteapress.com/thinkpython2/html/thinkpython2009.html#sec104
 
 '''
