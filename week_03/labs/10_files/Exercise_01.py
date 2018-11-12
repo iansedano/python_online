f2b969f5d4b4cd20f92bbd63c9746a85cd1cb89f
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-11-12 13:08:19 +0100

    files instructions update

--- week_03/labs/10_files/Exercise_01.py
+++ week_03/labs/10_files/Exercise_01.py
@@ -1,7 +1,7 @@
 '''
-Complete Exercise 9.1 (p.84) from the textbook.
+Write a program that reads in the file words.txt and prints only the
+words with more than 20 characters (not counting whitespace).
 
-Write a program that reads words.txt and prints only the words
-with more than 20 characters (not counting whitespace).
+Source: http://greenteapress.com/thinkpython2/html/thinkpython2010.html
 
 '''
