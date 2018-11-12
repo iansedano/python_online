a08e78d17cf9744c9fcfaeae1a779cd2b133feff
Caden MacKenzie <cadenmackenzie@gmail.com>
2018-11-12 13:07:36 +0100

    tuple instructions update

--- week_03/labs/08_tuples/Exercise_02.py
+++ week_03/labs/08_tuples/Exercise_02.py
@@ -1,7 +1,7 @@
 '''
 Complete the task in section 12.4 - Variable-length argument tuples - (p. 118).
 
-Write a function called sumall that takes any number of arguments
+Write a function called sumall that takes any number of int arguments
 and returns their sum.
 
 '''
